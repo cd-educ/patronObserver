63410eb10968921b286008e51e45957851e3ab57
Christian Nievas <cdnievas@hotmail.com>
2019-04-16 19:14:43 -0300

    Initial commit

---
+++ OurTube/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OurTube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Creo usuarios
+            Usuario user1 = new Usuario("pepit0123");
+            Usuario user2 = new Usuario("JuancitoXxGamerxX");
+            Usuario user3 = new Usuario("TutorialesByPedro");
+            Usuario user4 = new Usuario("ElRubiusxD");
+
+            // Los subscribo a ElRubius
+            user1.subscribirseA(user4);
+            user2.subscribirseA(user4);
+            user3.subscribirseA(user4);
+
+            // Subo video
+            user4.subirVideo("Jugando al MineCraft");
+
+            // Pruebo que el usuario que sube un video tambien puede recibir notificaciones
+            user4.subscribirseA(user3);
+            user3.subirVideo("Como usar Photoshop | Parte 1");
+
+            // Trabo la consola para poder leer los resultado
+            Console.ReadLine();
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# patronObserver
+# Patrón Observer
+Ejemplo del patrón Observer
+
+## Consigna
+
+Nos piden desde OurTube poder generar un sistema de subscripciones como usa la compañía contraria, siendo este un sistema que avise a un cierto usuario cuando otros suban un video.
+
+## Modelo Solución
+![Modelo](/modelo.png?raw=true)
+
